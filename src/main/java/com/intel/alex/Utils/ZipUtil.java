44393ee504dd68b82cf34352dd5f15584113302b
LifengWang <wanglifeng0123@163.com>
2016-03-14 23:19:15 -0400

    Update ZipUtil.java to unzip to project folder

--- src/main/java/com/intel/alex/Utils/ZipUtil.java
+++ src/main/java/com/intel/alex/Utils/ZipUtil.java
@@ -8,27 +8,30 @@ import net.lingala.zip4j.exception.ZipException;
  * Created by root on 16-2-19.
  */
 public class ZipUtil {
-    public String unzip(String zipFile, String destination) {
+//    public String unzip(String zipFile, String destination) {
+    public String unzip(String zipFile) {
+
         String[] s = zipFile.split("/");
         String[] s2 = s[s.length - 1].split(".zip");
+        //System.out.println("s2[0]="+s2[0]);
         String dir = s2[s2.length - 1];
         try {
             ZipFile zf = new ZipFile(zipFile);
-            zf.extractAll(destination);
+            zf.extractAll(s2[0]);
         } catch (ZipException e) {
             e.printStackTrace();
         }
-        System.out.println(destination + zipFile);
+        //System.out.println(s2[0] + zipFile);
 
-        return destination + "/" + dir;
+        return s2[0] + "/" + dir;
     }
 
-    public void unzip(String zipFile) {
-        try {
-            ZipFile zf = new ZipFile(zipFile);
-            zf.extractAll(this.getClass().getResource("").getPath());
-        } catch (ZipException e) {
-            e.printStackTrace();
-        }
-    }
+//    public void unzip(String zipFile) {
+//        try {
+//            ZipFile zf = new ZipFile(zipFile);
+//            zf.extractAll(this.getClass().getResource("").getPath());
+//        } catch (ZipException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
