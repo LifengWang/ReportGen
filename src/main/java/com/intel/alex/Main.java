b88c534fcb27fadb1719f9d05efefc611a28ca3c
LifengWang <wanglifeng0123@163.com>
2016-03-14 23:16:21 -0400

    Mofify Main.java for selective arguments

--- src/main/java/com/intel/alex/Main.java
+++ src/main/java/com/intel/alex/Main.java
@@ -18,16 +18,30 @@ public class Main {
         //get the BigBench log file and unzip destination from args
         GenReport gr = new GenReport();
         ZipUtil zu = new ZipUtil();
-        FileUtil fu = new FileUtil(zu.unzip(args[0], args[1]));
+        //FileUtil fu = new FileUtil(zu.unzip(args[0], args[1]));
+        FileUtil fu = new FileUtil(zu.unzip(args[0]));
+        //zu.unzip(args[0]);
         //System.out.println(zu.unzip(args[0], args[1]));
-        XMLUtil xu = new XMLUtil(args[2]);
+        XMLUtil xu = new XMLUtil(args[1]);
+        //ExcelUtil eu = new ExcelUtil(zu.unzip(args[0], args[1]));
+        ExcelUtil eu = new ExcelUtil(zu.unzip(args[0]));
         List<Map<String, String>> queryList = fu.parseQueryProperty();
         Map<String, Object> queryResult = fu.parseQueryResult();
         Map<String, Object> xmlMap = xu.parseXMLFile();
-        gr.createDoc(queryList, queryResult, xmlMap);
-
-        ExcelUtil eu = new ExcelUtil(zu.unzip(args[0], args[1]));
-        eu.createExcel();
-
+        if (args[2].equals("both")){
+            gr.createDoc(queryList, queryResult, xmlMap);
+            eu.createExcel();
+        }
+        else if (args[2].equals("doc")) {
+            gr.createDoc(queryList, queryResult, xmlMap);
+        }
+        else if(args[2].equals("xls")) {
+            eu.createExcel();
+        }
+        else{
+            System.out.println("For the third argument, please enter 'doc' to generate BigBench Report; ");
+            System.out.println("Enter 'xls' to generate excel file of elapse time for each query and phrase.");
+            System.out.println("Enter 'both' to generate both BigBench Report and excel file of elapse time for each query and phrase.");
+        }
     }
 }
